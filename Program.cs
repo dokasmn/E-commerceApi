a3b16238f3cb132077eba13d365e3aedbee66ed2
Moreira Cauan (SO/BUD3-ETS) <novocsm@gmail.com>
2024-09-30 11:28:25 -0300

    Feat: adicionando endpoints de produto adicionando endpoints extras de editar, deletar e retornar um produto específico

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ using Swashbuckle.AspNetCore.SwaggerUI;
 var builder = WebApplication.CreateBuilder(args); // cria um construtor da aplicação
 
 /*
+    // comandos para cria um banco de dados na memória
     builder.Services.AddDbContext<EcommerceDb>(opt => opt.UseInMemoryDatabase("Ecommerce")); // adiciona um contexto de banco de dados na memória
     builder.Services.AddDatabaseDeveloperPageExceptionFilter(); // adiciona um filtro de exceção
 */
 
+// comandos para se conectar com um banco de dados MySql existente
 builder.Services.AddDbContext<EcommerceDb>(options =>
     options.UseMySql("Server=localhost;Database=Ecommerce;User=root;Password=root;", 
     new MySqlServerVersion(new Version(8, 0, 25))));
@@ -37,9 +39,19 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-// cria um endpoint get para a aplicação
+
+// cria um endpoint get que retorna todos os produtos
 app.MapGet("/ecommerce", async (EcommerceDb db) =>
     await db.Products.ToListAsync());
+
+
+// cria um endpoint que retorna um produto específico
+app.MapGet("/ecommerce/{id}", async (int id, EcommerceDb db) =>
+    await db.Products.FindAsync(id)
+        is Product product
+            ? Results.Ok(product)
+            : Results.NotFound());
+
 
 // endpoint para inserir um novo produto
 app.MapPost("/ecommerce", async (EcommerceDb db, Product product) =>
@@ -48,5 +60,46 @@ app.MapPost("/ecommerce", async (EcommerceDb db, Product product) =>
     await db.SaveChangesAsync(); // Salva as alterações no banco de dados em memória
     return Results.Created($"/ecommerce/{product.ProductId}", product); // Retorna o produto criado
 });
+
+
+// endpoint para atualizar um produto
+app.MapPut("/ecommerce/{id}", async (int id, Product inputProduct, EcommerceDb db) =>
+{
+    var product = await db.Products.FindAsync(id);
+    if (product is null) return Results.NotFound();
+
+    product.ProductTitle = inputProduct.ProductTitle;
+    if (inputProduct.ProductDescription is not null)
+    {
+        product.ProductDescription = inputProduct.ProductDescription;
+    }
+    product.ProductPrice = inputProduct.ProductPrice;
+    if (inputProduct.ProductThumbnail is not null)
+    {
+        product.ProductThumbnail = inputProduct.ProductThumbnail;
+    }
+    if (inputProduct.ProductIsFeatured is not null)
+    {
+        product.ProductIsFeatured = inputProduct.ProductIsFeatured;  
+    }
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+
+// endpoint para deletar um produto
+app.MapDelete("/ecommerce/{id}", async (int id, EcommerceDb db) =>
+{
+    if (await db.Products.FindAsync(id) is Product product)
+    {
+        db.Products.Remove(product);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
 
 app.Run();
